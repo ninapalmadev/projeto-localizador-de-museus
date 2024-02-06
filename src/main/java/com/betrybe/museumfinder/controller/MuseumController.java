ef2d176fa032dee24fbd5251178bf65b3c74fe14
Nina Palma <ninapalmadev@gmail.com>
2024-02-06 19:45:17 -0300

    Requisito 5: Cria classe controller para /museums com rota POST

---
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -0,0 +1,35 @@
+package com.betrybe.museumfinder.controller;
+
+import com.betrybe.museumfinder.dto.MuseumCreationDto;
+import com.betrybe.museumfinder.dto.MuseumDto;
+import com.betrybe.museumfinder.model.Museum;
+import com.betrybe.museumfinder.service.MuseumServiceInterface;
+import com.betrybe.museumfinder.util.ModelDtoConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * MuseumController.
+ */
+@RestController
+@RequestMapping("/museums")
+public class MuseumController {
+
+  private final MuseumServiceInterface service;
+
+  @Autowired
+  public MuseumController(MuseumServiceInterface service) {
+    this.service = service;
+  }
+
+  @PostMapping
+  public ResponseEntity<MuseumDto> createMuseum(@RequestBody MuseumCreationDto newMuseum) {
+    Museum museum = service.createMuseum(ModelDtoConverter.dtoToModel(newMuseum));
+    return ResponseEntity.status(HttpStatus.CREATED).body(ModelDtoConverter.modelToDto(museum));
+  }
+}
